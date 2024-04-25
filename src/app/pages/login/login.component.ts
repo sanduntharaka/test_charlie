695a364e84c1a4ea97572ed3795839427edc76b0
Dulanga <dulanga.perera@soluvia.io>
2024-04-25 19:47:48 +0200

    sync

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  user?: string
+  password?: string
+  loading = false
 
+  onLogin() {
+    this.loading = true
+  }
 }
