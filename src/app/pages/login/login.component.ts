12507b2c54442025c5390e05b9f649596da26b5f
Dulanga <dulanga.perera@soluvia.io>
2024-04-25 21:04:13 +0200

    sync

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { OdooEntityManager } from '../../shared/services/odoo-entity-manager.service';
+import { OdoorpcService } from '../../shared/services/odoorpc.service';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
@@ -14,8 +17,18 @@ export class LoginComponent {
   password?: string
   loading = false
 
+  constructor(private odooRPC: OdoorpcService, private router: Router) {}
+
   onLogin() {
     this.loading = true
+    if (this.user && this.password) {
+      this.odooRPC.login({db: 'amanda', login: this.user, password: this.password})
+      if (this.odooRPC.logged) {
+        this.router.navigate(['time-card'])
+      }
+    }
+    
+
   }
-  
+
 }
--- src/app/pages/time-card/time-card.component.ts
+++ src/app/pages/time-card/time-card.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-time-card',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './time-card.component.html',
   styleUrl: './time-card.component.scss'
 })
