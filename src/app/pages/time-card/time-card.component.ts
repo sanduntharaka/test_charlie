d816f2083b616c8a9049bec70bc75cb8b6a192b0
Dulanga <dulanga.perera@soluvia.io>
2024-04-25 21:14:02 +0200

    sync

--- src/app/pages/time-card/time-card.component.ts
+++ src/app/pages/time-card/time-card.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { OdoorpcService } from '../../shared/services/odoorpc.service';
+import { OdooEntityManager } from '../../shared/services/odoo-entity-manager.service';
 
 @Component({
   selector: 'app-time-card',
@@ -8,6 +10,15 @@ import { RouterLink } from '@angular/router';
   templateUrl: './time-card.component.html',
   styleUrl: './time-card.component.scss'
 })
-export class TimeCardComponent {
+export class TimeCardComponent implements OnInit{
+
+  display_name = ""
+
+  constructor(private odooRpc: OdoorpcService, odooEm: OdooEntityManager) {}
+
+  async ngOnInit(): Promise<void> {
+   let info = await this.odooRpc.getSessionInfo()
+   this.display_name = info.result.name
+  }
 
 }
