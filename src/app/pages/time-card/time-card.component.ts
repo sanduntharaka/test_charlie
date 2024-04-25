407b198f20c4b44160c62da16574e52b2bc111cf
Dulanga Perera <dulangaperera@dmacbookpro3.station>
2024-04-25 19:26:34 +0200

    sync

---
+++ src/app/pages/time-card/time-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-time-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './time-card.component.html',
+  styleUrl: './time-card.component.scss'
+})
+export class TimeCardComponent {
+
+}
